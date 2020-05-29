8706e17487618ebae7fa58fb03f4c086434d69b2
Baptiste Drillien <baptiste.drillien@gmail.com>
2020-05-29 15:23:56 +0200

    Updates

---
+++ precache-manifest.018a014c52dfec70bcde9d4063209056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22580773fd3b8ddfa1b7fd784bcbdaff",
+    "url": "/space_launches/index.html"
+  },
+  {
+    "revision": "67849e8c1168151c4252",
+    "url": "/space_launches/static/css/main.c5662958.chunk.css"
+  },
+  {
+    "revision": "e844bc646209df66b579",
+    "url": "/space_launches/static/js/2.5ad01493.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space_launches/static/js/2.5ad01493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67849e8c1168151c4252",
+    "url": "/space_launches/static/js/main.714ec8f4.chunk.js"
+  },
+  {
+    "revision": "03988f3cb0f3ccb5a8d5",
+    "url": "/space_launches/static/js/runtime-main.63fe3025.js"
+  }
+]);
